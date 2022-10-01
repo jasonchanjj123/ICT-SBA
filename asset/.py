811ada5c6f5ee8ac24b6eea978a96816ea02c684
jasonchanjj123 <80382634+jasonchanjj123@users.noreply.github.com>
2022-10-01 10:15:20 +0000

    table update

--- asset/.py
+++ asset/.py
@@ -1,10 +1,14 @@
 import json
 
-file=input(">>")
+file = "asset/"+input(">>")
 
-with open(file,'rb')as f:
+with open(file,'r+',encoding="utf-8")as f:
     data = f.read()
-    open(file[0:-4]+'.json','w') #create json file
-    length = len(data.split('\n'))
-    for l in range(length):
-        
+    new_f = open(file[0:-4]+'.json','w',encoding="utf-8") #create json file
+    table = data.split('\n')
+    length = len(table)
+    list_=[[[int(table[l][num+l2*9])  for num in range(9)]for l2 in range(9)]for l in range(length-1)]
+    r={}
+    r["test"]=list_
+    json.dump(r,new_f)
+
